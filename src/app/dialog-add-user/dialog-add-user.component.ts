79885e52d5312ae36702d1cb7da23bcf26f43fe1
GökaySahin <goekay.sahin@gmx.net>
2023-08-25 23:13:05 +0200

    add popup to add user

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { MatDialogModule } from "@angular/material/dialog";
+
+@Component({
+  selector: "app-dialog-add-user",
+  templateUrl: "./dialog-add-user.component.html",
+  styleUrls: ["./dialog-add-user.component.scss"],
+})
+export class DialogAddUserComponent {}
